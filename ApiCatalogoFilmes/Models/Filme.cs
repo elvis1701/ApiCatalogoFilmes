140ebb72bd7a041e5b9d029ce8a09ef8e3d05c87
Elvis Alves <elvis.alves54@gmail.com>
2021-08-16 21:18:19 -0300

    creation of classes

---
+++ ApiCatalogoFilmes/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoFilmes.Models
+{
+    public class Filme
+    {
+        public int FilmeId { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Ano { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public Genero Genero { get; set; }
+
+        public int GeneroId { get; set; }
+
+
+    }
+}
---
+++ ApiCatalogoFilmes/Models/Genero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoFilmes.Models
+{
+    public class Genero
+    {
+        public int GeneroId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Imagem { get; set; }
+
+        public ICollection<Filme> Filmes { get; set; }
+    }
+}
